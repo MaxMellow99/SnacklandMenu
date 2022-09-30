9b0aa0b2edbac3af74c91ca42cf73b6e15d8d701
Timguse <tguse@casema.nl>
2022-09-30 16:17:01 +0200

    update

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 	require_once "database.php";
 	include "./managers/foodManager.php";
 
-	$food = foodmanager::select();
+	$foods = foodmanager::select();
 ?>
 <html>
 	<body>
-		<div class="menu"></div>
+		<div class="Patat">
+			<table>
+				<thead>
+					<th>name</th>
+				</thead>
+				<tbody>
+					<?php
+						foreach($foods as $food){
+							echo "<td></td>";
+							echo "<td></td>";
+						}
+					?>
+				</tbody>
+			</table>
+		</div>
 	</body>
 </html>
--- managers/foodManager.php
+++ managers/foodManager.php
@@ -2,7 +2,18 @@
     class foodmanager{
         public static function select(){
             global $con;
+
+            $stmt = $con->prepare("SELECT * FROM food");
+            $stmt->execute();
+
+            return $stmt -> fetchObject();       
+         }
+        public static function insert(){
+            global $con;
+
+        }
+        public static function update(){
+            global $con;
+
         }
     }
-
-?>
